2fc9038811e05a8913cb6746e6036194d3c53523
Wits.TommyLee@wistron.com <tommy6300167@gmail.com>
2023-12-13 15:17:35 +0800

    first commit

---
+++ routes/blogRoutes.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
+const cleanCache = require("../middlewares/cleanCache");
+const Blog = mongoose.model("Blog");
+
+module.exports = (app) => {
+  app.get("/api/blogs/:id", requireLogin, async (req, res) => {
+    const blog = await Blog.findOne({
+      _user: req.user.id,
+      _id: req.params.id,
+    });
+
+    res.send(blog);
+  });
+
+  app.get("/api/blogs", requireLogin, async (req, res) => {
+    const userId = req.user.id;
+    const blogs = await Blog.find({ _user: userId }).cache({ key: userId });
+
+    res.send(blogs);
+  });
+
+  app.post("/api/blogs", requireLogin, cleanCache, async (req, res) => {
+    const { title, content } = req.body;
+
+    const blog = new Blog({
+      title,
+      content,
+      _user: req.user.id,
+    });
+
+    try {
+      await blog.save();
+      res.send(blog);
+    } catch (err) {
+      res.send(400, err);
+    }
+  });
+};
---
+++ tests/setup.js
@@ -0,0 +1,17 @@
+// wait 30 second when fail the test
+jest.setTimeout(30000);
+
+require("../models/User");
+
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+});
