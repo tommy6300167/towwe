028d449a49baeee8ba353fc46ddebbcc8eb91776
Wits.TommyLee@wistron.com <tommy6300167@gmail.com>
2023-12-13 15:49:14 +0800

    add ci env

--- services/cache.js
+++ services/cache.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 const redis = require("redis");
+const keys = require("../config/keys");
 
 const util = require("util");
 
-const redisUrl = "redis://127.0.0.1:6380";
-const client = redis.createClient(redisUrl);
+const client = redis.createClient(keys.redisUrl);
 client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
